49e7d7f882e29d614efb8c9eab132b30f389d100
ArtAnt00 <88104295+ArtAnt00@users.noreply.github.com>
2023-01-20 22:55:35 +0700

    Update log.py

--- log.py
+++ log.py
@@ -4,18 +4,16 @@ import re
 import asyncio
 from telegram import Bot
 
-# При использовании python выше версии 3.8 библиотека telegram  может не работать тогда вам нужно "pip unistall telegram" и вместо этого сделать
-# pip install python-telegram-bot
 async def send_message(chat_id, telegram_message, bot_token):
     bot = Bot(token=bot_token)
     await bot.send_message(chat_id=chat_id, text=telegram_message)
 # List of words to search for
 words_to_search = ['WARNING','ERROR','ls -la' ] #Вы можете добавить любые слова для поиска в ваших логах
 
-# The log file to analyze on the remote system
+# путь до файла логов на удаленном сервере
 log_file = '/dir/log/logfile.log'
 
-# Connect to the remote system
+# Подключение к удаленному серверу
 hostname = "хост"
 username = "имя пользователя"
 password = "пароль"
@@ -27,14 +25,14 @@ try:
 except Exception as e:
     print(f'Error  SSH connection: {e}')
 
-# Read the log file on the remote system
+# Прочитайте файл журнала в удаленной системе
 stdin, stdout, stderr = ssh.exec_command(f"cat {log_file}")
 log_lines = stdout.readlines()
 
-# Search for each word in the log file
+# Поиск каждого слова в файле журнала
 telegram_message = "Warn: "
 
-# Iterate through the words
+# Повторяйте слова
 for word in words_to_search:
     for i, line in enumerate(log_lines):
         match = re.search(word, line)
@@ -44,11 +42,11 @@ for word in words_to_search:
 
 if telegram_message != "Warn: ":
     try:
-        # Telegram bot details
+        # Телеграм бот данные
         bot_token = "ВАШ ТОКЕН БОТА"
         chat_id = "ЧАТ ИД"
 
-        # Send message to Telegram
+        #отправка сообщения в телеграмм
         message_chunks = [telegram_message[i:i+4096] for i in range(0, len(telegram_message), 4096)]
         for message in message_chunks:
             asyncio.run(send_message(chat_id, message, bot_token))
@@ -58,7 +56,7 @@ if telegram_message != "Warn: ":
         print(f'Error sending Telegram message: {e}')
 
 try:
-    # Close the SSH connection
+    # Закрытие ssh подключения
     ssh.close()
 except Exception as e:
     print(f'Error closing SSH connection: {e}')
